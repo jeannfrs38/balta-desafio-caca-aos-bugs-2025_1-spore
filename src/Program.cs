418ef3d98e868a483642e0d1fdbd8b90e6cd8ccd
Jean felipe <jeanfrs38@gmail.com>
2025-10-25 21:19:19 -0300

    feat: Finalizado Primeiro Desafio

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,13 @@
+using BugStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+var connctString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(x => x.UseSqlite(connctString));
 var app = builder.Build();
+
+
 
 app.MapGet("/", () => "Hello World!");
 
